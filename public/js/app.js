312788a2003d11cba13aee44606ce566a2457aa8
Ben Ripkens <bripkens.dev@gmail.com>
2013-05-03 16:22:21 +0200

    First routes, fallback and a small easter egg.

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+angular.module('MovieDatabase', []).config(function ($routeProvider) {
+    'use strict';
+
+    $routeProvider
+    .when('/', {
+        controller: WelcomeCtrl,
+        templateUrl: 'partial/index.html'
+    })
+    .when('/404', {
+        controller: NotFoundCtrl,
+        templateUrl: 'partial/notFound.html'
+    })
+    .otherwise({ redirectTo: '/404' });
+
+
+});
---
+++ public/js/controller.js
@@ -0,0 +1,9 @@
+function AppCtrl ($scope) {
+    $scope.title = 'The Movie Database';
+}
+
+function WelcomeCtrl () {
+}
+
+function NotFoundCtrl () {
+}
