d0e72dc59e9c99d6fefcb22ac9f8b38817e2ad41
Ben Ripkens <bripkens.dev@gmail.com>
2013-05-07 20:53:02 +0200

    Eror handling on the server side and movie detail web service endpoint.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,34 +4,54 @@ var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase(
     process.env.NEO4J_URL || 'http://localhost:7474');
 
+
 function getAbsoluteUriBase (req) {
     // we use req.get('host') as this also gives us the port
     return req.protocol + '://' + req.get('host');
 }
 
+
 exports.hello = function (req, res) {
     res.send('Hello World - now automatically deployed!');
 };
 
+
 exports.movies = function (req, res) {
-    db.getIndexedNodes(
-        'node_auto_index', 'type', 'movie',
-        function (err, nodes) {
-            console.log(nodes);
-            var movies = nodes.map(function (node) {
-                return node.data;
-            });
-            res.send(movies);
+    db.getIndexedNodes('node_auto_index', 'type', 'movie',
+            function (err, nodes) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send();
+        }
+
+        // fallback in case no movies are stored in the database
+        nodes = nodes || [];
+
+        var movies = nodes.map(function (node) {
+            return node.data;
         });
+
+        res.send(movies);
+    });
 };
+
 
 exports.getMovie = function (req, res) {
     var id = req.params.id;
     // TODO how to retrieve movie => UUID?
-    res.send({
-        title: 'Foobar'
+
+    db.getIndexedNode('node_auto_index', 'id', id, function (err, node) {
+        if (err) {
+            res.status(500).send();
+            console.error(err);
+        } else if (!node) {
+            res.status(404).send();
+        } else {
+            res.send(node.data);
+        }
     });
 };
+
 
 exports.deleteMovie = function (req, res) {
     var id = req.params.id;
@@ -40,14 +60,15 @@ exports.deleteMovie = function (req, res) {
     res.status(204).send();
 };
 
+
 exports.addMovie = function (req, res) {
     var node = db.createNode(req.body);
     node.data.type = 'movie';
     node.data.id = uuid.v4();
-    node.save(function (error, savedNode) {
-        if (error) {
-            res.status(500).send();
-            console.error(error);
+    node.save(function (err, savedNode) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send();
         }
 
         res.status(201)
