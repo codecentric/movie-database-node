5023b827330451ff48ca67670555854d532f4dac
Ben Ripkens <bripkens.dev@gmail.com>
2013-07-15 20:49:38 +0200

    chore(Test data): Add more test data for the integration tests

--- test/karma/integration/index.spec.js
+++ test/karma/integration/index.spec.js
@@ -37,19 +37,44 @@ describe('Movies', function () {
         expect(browser().window().path()).toEqual('/movies/new');
     });
 
-    it('should add movies and forward to the detail view', function () {
-        var title = 'Batman: The Dark Knight';
-        var description = 'When Batman, Gordon and Harvey Dent...';
-
-        // add movie
+    function addMovie (title, description) {
         browser().navigateTo(addMovieUrl);
         input('movie.title').enter(title);
         input('movie.description').enter(description);
         element('.btn-primary').click();
+    }
 
-        // make sure it has been saved
-        expect(browser().window().path()).toMatch(/^\/movies\/.*$/);
-        expect(element('h2').text()).toEqual(title);
+    function addTestMovies () {
+        // The short description of each of the following movies was copied
+        // from the IMDB. [Insert further copyright notices here] ;-)
+
+        addMovie('The Dark Knight', 'When Batman, Gordon and Harvey Dent ' +
+            'launch an assault on the mob, they let the clown out of the ' +
+            'box, the Joker, bent on turning Gotham on itself and bringing ' +
+            'any heroes down to his level.');
+        addMovie('The Dark Knight Rises', 'Eight years on, a new evil rises ' +
+            'from where the Batman and Commissioner Gordon tried to bury it, ' +
+            'causing the Batman to resurface and fight to protect Gotham ' +
+            'City... the very city which brands him an enemy.');
+        addMovie('Cloud Atlas', 'An exploration of how the actions of ' +
+            'individual lives impact one another in the past, present and ' +
+            'future, as one soul is shaped from a killer into a hero, and ' +
+            'an act of kindness ripples across centuries to inspire a ' +
+            'revolution.');
+        addMovie('Ted', 'As the result of a childhood wish, John Bennett\'s ' +
+            'teddy bear, Ted, came to life and has been by John\'s side ever ' +
+            'since - a friendship that\'s tested when Lori, John\'s ' +
+            'girlfriend of four years, wants more from their relationship.');
+        addMovie('The Meerkats', 'An inspiring exploration of one family\'s ' +
+            'resilience and fortitude shot using innovative and ' +
+            'groundbreaking filming techniques.');
+        addMovie('Undisputed II: Last Man Standing', 'Sequel to the 2002 ' +
+            'film. This time, Heavyweight Champ George "Iceman" Chambers is ' +
+            'sent to a Russian Jail on trumped-up drug charges.');
+    }
+
+    it('should add movies and forward to the detail view', function () {
+        addTestMovies();
 
         // look for it on the overview page
         browser().navigateTo(baseUrl);
