a325d1f899fd8999b212e1a93f47519e2322ed63
Michael Lex <mich.lex@gmail.com>
2013-09-06 13:24:40 +0200

    improve(karma): Update to new karma version 0.10.2.

--- test/karma/integration.conf.js
+++ test/karma/integration.conf.js
@@ -1,76 +1,81 @@
-// base path, that will be used to resolve files and exclude. Path is relative
-// to the configuration file
-basePath = '../..';
+module.exports = function(config) {
+  config.set({
 
-frameworks = ['ng-scenario'];
+    // base path, that will be used to resolve files and exclude. Path is relative
+    // to the configuration file
+    basePath: '../..',
+    
+    frameworks: ['ng-scenario'],
+    
+    // list of files / patterns to load in the browser
+    files : [
+      'test/karma/integration/**/*.spec.js'
+    ],
+    
+    // list of files to exclude
+    exclude : [],
+    
+    // use dolts reporter, as travis terminal does not support escaping sequences
+    // possible values: 'dots', 'progress', 'junit', 'teamcity'
+    // CLI --reporters progress
+    reporters : ['spec'],
 
-// list of files / patterns to load in the browser
-files = [
-    'test/karma/integration/**/*.spec.js'
-];
+    // web server port
+    // CLI --port 9876
+    port : 9876,
+    
+    // cli runner port
+    // CLI --runner-port 9100
+    runnerPort : 9100,
+    
+    // enable / disable colors in the output (reporters and logs)
+    // CLI --colors --no-colors
+    colors : true,
 
-// list of files to exclude
-exclude = [];
+    // level of logging
+    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+    // CLI --log-level debug
+    logLevel : LOG_INFO,
 
-// use dolts reporter, as travis terminal does not support escaping sequences
-// possible values: 'dots', 'progress', 'junit', 'teamcity'
-// CLI --reporters progress
-reporters = ['progress'];
+    // enable / disable watching file and executing tests whenever any file changes
+    // CLI --auto-watch --no-auto-watch
+    autoWatch : true,
 
-// web server port
-// CLI --port 9876
-port = 9876;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    // CLI --browsers Chrome,Firefox,Safari
+    browsers : ['Firefox'],
 
-// cli runner port
-// CLI --runner-port 9100
-runnerPort = 9100;
+    // If browser does not capture in given timeout [ms], kill it
+    // CLI --capture-timeout 5000
+    captureTimeout : 5000,
 
-// enable / disable colors in the output (reporters and logs)
-// CLI --colors --no-colors
-colors = true;
+    // Auto run tests on start (when browsers are captured) and exit
+    // CLI --single-run --no-single-run
+    singleRun : true,
 
-// level of logging
-// possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
-// CLI --log-level debug
-logLevel = LOG_INFO;
+    // report which specs are slower than 1000ms
+    // CLI --report-slower-than 1000
+    reportSlowerThan : 1000,
 
-// enable / disable watching file and executing tests whenever any file changes
-// CLI --auto-watch --no-auto-watch
-autoWatch = true;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-// CLI --browsers Chrome,Firefox,Safari
-browsers = ['Firefox'];
-
-// If browser does not capture in given timeout [ms], kill it
-// CLI --capture-timeout 5000
-captureTimeout = 5000;
-
-// Auto run tests on start (when browsers are captured) and exit
-// CLI --single-run --no-single-run
-singleRun = true;
-
-// report which specs are slower than 1000ms
-// CLI --report-slower-than 1000
-reportSlowerThan = 1000;
-
-plugins = [
-    'karma-ng-scenario',
-    'karma-chrome-launcher',
-    'karma-firefox-launcher',
-    'karma-phantomjs-launcher',
-    'karma-spec-reporter'
-];
-
-urlRoot = '/__karma/';
-
-proxies = {
-    "/": "http://localhost:3000/"
+    plugins : [
+      'karma-ng-scenario',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-spec-reporter'
+    ],
+    
+    urlRoot : '/__karma/',
+    
+    proxies : {
+      "/": "http://localhost:3000/"
+    }
+  });
 };
--- test/karma/unit.conf.js
+++ test/karma/unit.conf.js
@@ -1,76 +1,83 @@
-// base path, that will be used to resolve files and exclude
-basePath = '../..';
+module.exports = function(config) {
+  config.set({
 
-frameworks = ['mocha'];
+    // base path, that will be used to resolve files and exclude
+    basePath : '../..',
 
-// list of files / patterns to load in the browser
-files = [
-    'node_modules/chai/chai.js',
-    'client/js/lib/angular.js',
-    'client/js/**/*.js',
-    'test/karma/unit/**/*.spec.js'
-];
+    frameworks : ['mocha'],
 
-// list of files to exclude
-exclude = [];
+    // list of files / patterns to load in the browser
+    files : [
+      'node_modules/chai/chai.js',
+      'client/js/lib/angular.js',
+      'client/js/**/*.js',
+      'test/karma/unit/**/*.spec.js'
+    ],
 
-// use dolts reporter, as travis terminal does not support escaping sequences
-// possible values: 'dots', 'progress', 'junit', 'teamcity'
-// CLI --reporters progress
-reporters = ['spec'];
+    // list of files to exclude
+    exclude : [],
 
-// web server port
-// CLI --port 9876
-port = 9876;
+    // use dolts reporter, as travis terminal does not support escaping sequences
+    // possible values: 'dots', 'progress', 'junit', 'teamcity'
+    // CLI --reporters progress
+    reporters : ['spec'],
 
-// cli runner port
-// CLI --runner-port 9100
-runnerPort = 9100;
+    // web server port
+    // CLI --port 9876
+    port : 9876,
 
-// enable / disable colors in the output (reporters and logs)
-// CLI --colors --no-colors
-colors = true;
+    // cli runner port
+    // CLI --runner-port 9100
+    runnerPort : 9100,
 
-// level of logging
-// possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
-// CLI --log-level debug
-logLevel = LOG_DEBUG;
+    // enable / disable colors in the output (reporters and logs)
+    // CLI --colors --no-colors
+    colors : true,
 
-// enable / disable watching file and executing tests whenever any file changes
-// CLI --auto-watch --no-auto-watch
-autoWatch = true;
+    // level of logging
+    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+    // CLI --log-level debug
+    logLevel : config.LOG_DEBUG,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-// CLI --browsers Chrome,Firefox,Safari
-browsers = ['Firefox'];
+    // enable / disable watching file and executing tests whenever any file changes
+    // CLI --auto-watch --no-auto-watch
+    autoWatch : true,
 
-// If browser does not capture in given timeout [ms], kill it
-// CLI --capture-timeout 5000
-captureTimeout = 5000;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    // CLI --browsers Chrome,Firefox,Safari
+    browsers : ['Firefox'],
 
-// Auto run tests on start (when browsers are captured) and exit
-// CLI --single-run --no-single-run
-singleRun = true;
+    // If browser does not capture in given timeout [ms], kill it
+    // CLI --capture-timeout 5000
+    captureTimeout : 5000,
 
-// report which specs are slower than 500ms
-// CLI --report-slower-than 500
-reportSlowerThan = 500;
+    // Auto run tests on start (when browsers are captured) and exit
+    // CLI --single-run --no-single-run
+    singleRun : true,
 
-preprocessors = {
-//    '**/client/js/*.js': 'coverage'
+    // report which specs are slower than 500ms
+    // CLI --report-slower-than 500
+    reportSlowerThan : 500,
+
+    preprocessors : {
+      //    '**/client/js/*.js': 'coverage'
+    },
+
+    plugins : [
+      'karma-mocha',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-spec-reporter'
+    ],
+
+    // your config
+  });
 };
-
-plugins = [
-    'karma-mocha',
-    'karma-chrome-launcher',
-    'karma-firefox-launcher',
-    'karma-phantomjs-launcher',
-    'karma-spec-reporter'
-];
