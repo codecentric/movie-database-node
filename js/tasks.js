4d3fe27506cd586ef90e0ea93a989033a262c70e
Bastian Krol <bastian.krol@web.de>
2015-06-28 01:36:21 +0200

    group tasks into tracks

--- js/tasks.js
+++ js/tasks.js
@@ -36,13 +36,26 @@ module.controller('TaskCtrl', function TaskCtrl($scope, $http) {
 
   $http.get('./tasks.yml')
   .then(function(response) {
-    var tasks = jsyaml.load(response.data);
-    tasks.forEach(function(task, index) {
-      task.id = index+1
-      task.status = getStatus(task.id);
-      task.visible = false;
+    var tracks = [];
+    var trackWrappers = jsyaml.load(response.data);
+    var trackIndex = 0;
+    trackWrappers.forEach(function(trackWrapper) {
+      // a track wrapper is an object with only one attribute, the key is the
+      // name of the track, the value is the array of tasks.
+      Object.keys(trackWrapper).forEach(function(trackName) {
+        var tasks = trackWrapper[trackName];
+        tasks.forEach(function(task, index) {
+          task.id = trackName + '_' + (index);
+          task.status = getStatus(task.id);
+          task.visible = false;
+        });
+        tracks.push({
+          name: trackName,
+          tasks: tasks,
+        });
+      });
     });
-    $scope.tasks = tasks;
+    $scope.tracks = tracks;
   }, function(err) {
     alert('Failed to load the tasks, sorry!', JSON.stringify(err));
   });
