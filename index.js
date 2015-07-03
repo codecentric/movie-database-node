49a86e613d8f7f2b456167a33585f25feade335c
Michael Lex <mich.lex@gmail.com>
2015-07-03 10:00:48 +0200

    Cleanup.

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ angular.module('AgileJsDashboard', [])
 .value('REPOS', ['agilejs/2015-06-team-1', 'agilejs/2015-06-team-2'])
 .value('TRAVIS_AUTH_TOKEN', 'SECRET')
 
-.filter('chunk', function() {
-  return
-})
-
 .controller('AppController', ['$scope', 'REPOS', function($scope, REPOS) {
   $scope.repos = REPOS
 }]);
